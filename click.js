3f26986b4fcdc9ab2f0081cd0b7b17188e2dd020
Amit kushwaha <amit01golu@gmail.com>
2026-02-17 21:47:54 +0530

    Update click.js

--- click.js
+++ click.js
@@ -1,54 +1,4 @@
-// const http = require ('http')
-
-// const server = http.createServer((req , res ) => {
-//   console.log(req.url);
-
-//   if(req === '/home'){
-//     res.write('<h1>WELCOME TO HOME> <h1>')
-//   }else if(req === 'men'){
-//     res.write('<h1>WELCOME TO MEN> <h1>')
-//   }else if(req === '/women'){
-//     res.write('<h1>WELCOME TO women> <h1>')
-//   }else if(req === '/Kids'){
-//     res.write('<h1>WELCOME TO KIDS> <h1>')
-//   }else if(req === '/Other'){
-//     res.write('<h1>WELCOME TO OTHER> <h1>')
-//   re  
-
-
-
-
-
-//   res.write( '    <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <title>SHOOPING SITE</title>
-// </head>
-// <body>
-//   <nav>
-//     <ul>
-//       <li>
-//         <h1><a href="/Home">HOME</a></h1>
-//         <h1><a href="/men">MEN</a></h1>
-//         <h1><a href="/women">WOMEN</a></h1>
-//         <h1><a href="/Kids">KIDS</a></h1>
-//         <h1><a href="/Other">OTHER</a></h1>
-//       </li>
-//     </ul>
-//   </nav>
-// </body>
-// </html> ')
-
-  
-// })
-
-// const port = 3001;
-// server.listen(port , () => {
-//   console.log(`Server is running http://localhost${port}`);
-  
-// })
-
-
+// create server
 
 const http = require('http');
 
@@ -110,3 +60,4 @@ const port = 3001;
 server.listen(port, () => {
   console.log(`Server is running http://localhost:${port}`);
 });
+
