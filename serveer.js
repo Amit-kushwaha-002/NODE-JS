db4a77c209f6e0bb93ab5b5839a5aca35c092cca
Amit kushwaha <amit01golu@gmail.com>
2026-04-05 16:26:25 +0530

    Update serveer.js

--- serveer.js
+++ serveer.js
@@ -1,21 +1,22 @@
 
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const path = require("path");
 
 const app = express();
-
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // open login page
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "login.html"));
 });
-
 // login logic
 app.post("/login", (req, res) => {
 
     const username = req.body.username;
     const password = req.body.password;
-
     // simple login check
     if(username === "amit" && password === "1234"){
         res.sendFile(path.join(__dirname, "views", "dashboard.html"));
